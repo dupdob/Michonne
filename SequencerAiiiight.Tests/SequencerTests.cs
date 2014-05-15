ffe62056068132260d5728d68588392f35bae11d
Thomas PIERRAIN <thomas@pierrain.net>
2014-05-16 01:15:52 +0200

    First version with first (failing) unit test for @dupdob 's sequencer.

---
+++ SequencerAiiiight.Tests/SequencerTests.cs
@@ -0,0 +1,89 @@
+﻿namespace SequencerAiiiight.Tests
+{
+    using System.Collections.Generic;
+    using System.Threading;
+
+    using NFluent;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SequencerTests
+    {
+        #region Constants
+
+        private const int SecondInMsec = 1000;
+
+        #endregion
+
+        #region Fields
+
+        private readonly object syncRoot = new object();
+
+        private List<int> result;
+
+        private AutoResetEvent sequenceFinished;
+
+        #endregion
+
+        #region SetUp/TearDown
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            this.sequenceFinished = new AutoResetEvent(false);
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            if (this.sequenceFinished != null)
+            {
+                this.sequenceFinished.Dispose();
+                this.sequenceFinished = null;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        [Test]
+        public void SequencerExecutesTasksInTheOrderOfTheirDispatch()
+        {
+            var sequencer = new Sequencer();
+            const int MaxNumber = 10000;
+            
+            this.result = new List<int>(MaxNumber);
+
+            // loads the sequencer
+            for (var i = 0; i < MaxNumber; i++)
+            {
+                var antiClosureSideEffectNumber = i;
+                sequencer.Dispatch(() =>
+                {
+                    lock (this.syncRoot)
+                    {
+                        this.result.Add(antiClosureSideEffectNumber);
+                    }
+                });
+            }
+
+            // Indicates the end of the sequence
+            sequencer.Dispatch(() => this.sequenceFinished.Set()) ;
+
+            // use an autoreset event here instead
+            Check.That(this.sequenceFinished.WaitOne(5 * SecondInMsec)).IsTrue();
+
+            // Checks that everything was properly processed in sequence
+            for (int k = 0; k < MaxNumber; k++)
+            {
+                Check.That(this.result[k]).IsEqualTo(k);
+            }
+        }
+
+        
+
+        #endregion
+    }
+}
---
+++ SequencerAiiiight/Sequencer.cs
@@ -0,0 +1,72 @@
+﻿namespace SequencerAiiiight
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    /// <summary>
+    /// Incorrect implementation of the Sequencer from Cyrille's blog (http://dupdob.wordpress.com/2014/05/09/the-sequencer-part-2/)
+    /// </summary>
+    public class Sequencer
+    {
+        #region Fields
+
+        private readonly object _lock = new Object();
+
+        private readonly Queue<Action> _pendingTasks = new Queue<Action>();
+
+        private bool _isRunning;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public void Dispatch(Action action)
+        {
+            // he he he... ;-)
+
+            // here we queue a call to our own logic
+            ThreadPool.QueueUserWorkItem(x => this.Run((Action)x), action);
+        }
+
+        #endregion
+
+        // run when the pool has available cpu time for us.
+
+        #region Methods
+
+        private void Run(Action action)
+        {
+            lock (this._lock)
+            {
+                if (this._isRunning)
+                {
+                    // we need to store and stop
+                    this._pendingTasks.Enqueue(action);
+                    return;
+                }
+                // ok, we can run
+                this._isRunning = true;
+            }
+
+            while (true)
+            {
+                // execute the next action
+                action();
+                // we check if others are available
+                lock (this._lock)
+                {
+                    if (this._pendingTasks.Count == 0)
+                    {
+                        this._isRunning = false;
+                        return;
+                    }
+                    // pop the next task
+                    action = this._pendingTasks.Dequeue();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
