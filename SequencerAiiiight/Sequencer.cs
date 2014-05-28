c5df29a649c9ab2cd73b5087a2c62cdb806ead13
Thomas PIERRAIN <thomas@pierrain.net>
2014-05-28 08:23:52 +0200

    Adds the Apache license + headers.

--- SequencerAiiiight/Sequencer.cs
+++ SequencerAiiiight/Sequencer.cs
@@ -1,36 +1,54 @@
-﻿namespace SequencerAiiiight
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="Sequencer.cs" company="">
+// //   Copyright 2014 Cyrille DUPUYDAUBY, Thomas PIERRAIN
+// //   Licensed under the Apache License, Version 2.0 (the "License");
+// //   you may not use this file except in compliance with the License.
+// //   You may obtain a copy of the License at
+// //       http://www.apache.org/licenses/LICENSE-2.0
+// //   Unless required by applicable law or agreed to in writing, software
+// //   distributed under the License is distributed on an "AS IS" BASIS,
+// //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// //   See the License for the specific language governing permissions and
+// //   limitations under the License.
+// // </copyright>
+// // --------------------------------------------------------------------------------------------------------------------
+namespace SequencerAiiiight
 {
     using System;
     using System.Collections.Generic;
+
     using SequencerAiiiight.Interfaces;
 
     /// <summary>
-    /// Allows to execute tasks asynchronously, but one by one and in the same order as they have been dispatched.
-    /// That means that two tasks from the same dispatcher can be executed by two different threads, but not in parallel. 
-    /// Sequencer requirements are presented here: 
+    ///     Allows to execute tasks asynchronously, but one by one and in the same order as they have been dispatched.
+    ///     That means that two tasks from the same dispatcher can be executed by two different threads, but not in parallel.
+    ///     Sequencer requirements are presented here:
     ///     http://dupdob.wordpress.com/2014/05/09/the-sequencer-part-2/
     ///     and
     ///     http://dupdob.wordpress.com/2014/05/14/sequencer-part-2-1/
-    /// <remarks>
-    ///     Even if using this dispatcher will allow you to erradicate locks from your application code, 
-    ///     this particular implementation of the sequencer is not a lock-free.
-    /// </remarks>
+    ///     <remarks>
+    ///         Even if using this dispatcher will allow you to erradicate locks from your application code,
+    ///         this particular implementation of the sequencer is not a lock-free.
+    ///     </remarks>
     /// </summary>
     public sealed class Sequencer : ISequencer
     {
         // TODO: Get rid of the Queue, implement lock free algo, etc.
+
         #region Fields
 
-        private readonly object syncRoot = new object();
-        private readonly IDispatcher rootDispatcher;
         private readonly Queue<Action> orderedDispatchedTasks = new Queue<Action>();
-        private long numberOfPendingTasksWhileRunning;
+        private readonly IDispatcher rootDispatcher;
+        private readonly object syncRoot = new object();
         private bool isRunning;
+        private long numberOfPendingTasksWhileRunning;
 
         #endregion
 
+        #region Constructors and Destructors
+
         /// <summary>
-        /// Initializes a new instance of the <see cref="Sequencer"/> class.
+        ///     Initializes a new instance of the <see cref="Sequencer" /> class.
         /// </summary>
         /// <param name="rootDispatcher">The root Dispatcher.</param>
         public Sequencer(IDispatcher rootDispatcher)
@@ -39,11 +57,13 @@
             this.numberOfPendingTasksWhileRunning = 0;
         }
 
+        #endregion
+
         #region Public Methods and Operators
 
         /// <summary>
-        /// Gives a task/action to the sequencer in order to execute it in an asynchronous manner, but respecting the
-        /// order of the dispatch, and without concurrency among the sequencer's tasks.
+        ///     Gives a task/action to the sequencer in order to execute it in an asynchronous manner, but respecting the
+        ///     order of the dispatch, and without concurrency among the sequencer's tasks.
         /// </summary>
         /// <param name="action"></param>
         public void Dispatch(Action action)
@@ -62,14 +82,20 @@
         #endregion
 
         /// <summary>
-        /// Task that has been dispatched by the sequencer and that should be executed by its root dispatcher.
+        ///     Task that has been dispatched by the sequencer and that should be executed by its root dispatcher.
         /// </summary>
         private sealed class SequencedTask
         {
+            #region Fields
+
             private readonly Sequencer sequencer;
 
+            #endregion
+
+            #region Constructors and Destructors
+
             /// <summary>
-            /// Initializes a new instance of the <see cref="SequencedTask"/> class.
+            ///     Initializes a new instance of the <see cref="SequencedTask" /> class.
             /// </summary>
             /// <param name="sequencer">The sequencer.</param>
             public SequencedTask(Sequencer sequencer)
@@ -77,8 +103,12 @@
                 this.sequencer = sequencer;
             }
 
+            #endregion
+
+            #region Public Methods and Operators
+
             /// <summary>
-            /// Executes this dispatched task.
+            ///     Executes this dispatched task.
             /// </summary>
             public void Execute()
             {
@@ -87,22 +117,21 @@
                 {
                     if (this.sequencer.isRunning)
                     {
-                        // we need to store and stop
+                        // We need to store and stop
                         this.sequencer.numberOfPendingTasksWhileRunning++;
                         return;
                     }
-                    
-                    action = this.sequencer.orderedDispatchedTasks.Dequeue(); 
 
-                    // ok, we can run
+                    // Ok, we can run
+                    action = this.sequencer.orderedDispatchedTasks.Dequeue();
                     this.sequencer.isRunning = true;
                 }
 
                 while (true)
                 {
-                    // execute the next action
+                    // Execute the next action
                     action();
-                    // we check if others tasks have to be executed during this round
+                    // We check if others tasks have to be executed during this round
                     lock (this.sequencer.syncRoot)
                     {
                         if (this.sequencer.numberOfPendingTasksWhileRunning == 0)
@@ -110,9 +139,9 @@
                             this.sequencer.isRunning = false;
                             return;
                         }
-                        
-                        // pop the next task
-                        if(this.sequencer.numberOfPendingTasksWhileRunning > 0)
+
+                        // Pop the next task we are allowed to execute now (for fairness with dispatcher's other tasks)
+                        if (this.sequencer.numberOfPendingTasksWhileRunning > 0)
                         {
                             this.sequencer.numberOfPendingTasksWhileRunning--;
                             action = this.sequencer.orderedDispatchedTasks.Dequeue();
@@ -120,6 +149,8 @@
                     }
                 }
             }
+
+            #endregion
         }
     }
 }
