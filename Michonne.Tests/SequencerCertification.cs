78a7c0df544923d0d311771523498395e5784c6d
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2014-06-30 23:12:29 +0200

    Made SequencerCertification generic to ease test writing
    Implemented Sequentiality tests

--- Michonne.Tests/SequencerCertification.cs
+++ Michonne.Tests/SequencerCertification.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Reflection;
+using System.Threading;
 using Michonne.Interfaces;
 using NFluent;
 using NUnit.Framework;
 
 namespace Michonne.Tests
 {
-    [TestFixture]
-    internal class SequencerCertification
+    [TestFixture(typeof(Sequencer))]
+    internal class SequencerCertification<T>
     {
         // specify here the 
-        private readonly Type _sequencerType = typeof (Sequencer);
+        private readonly Type _sequencerType = typeof (T);
 
         private class SynchroCall : IUnitOfExecution
         {
-            public int _doneTasks;
+            public int DoneTasks;
 
             #region IUnitOfExecution Members
 
@@ -22,7 +23,7 @@ namespace Michonne.Tests
 
             {
                 action();
-                this._doneTasks++;
+                this.DoneTasks++;
             }
 
             #endregion
@@ -48,13 +49,61 @@ namespace Michonne.Tests
         public void Should_Use_Provided_Unit_Of_Execution()
         {
             var synchExec = new SynchroCall();
+            var sequencer = this.BuildSequencer(synchExec);
+            sequencer.Dispatch( () => {});
+
+            Check.That(synchExec.DoneTasks).Equals(1);
+        }
+
+        [Test]
+        public void Should_Execute_Tasks_Non_Concurrently()
+        {
+            var poolExec = new DotNetThreadPoolUnitOfExecution();
+            var sequencer = this.BuildSequencer(poolExec);
+            var lck = new object();
+            var done = false;
+            var success = true;
+            // first task inject delay
+            sequencer.Dispatch(() => { lock(lck) Thread.Sleep(30);});
+            // second task check non concurrence
+            sequencer.Dispatch(() =>
+            {
+                if (Monitor.TryEnter(lck))
+                {
+                    try
+                    {
+                        done = true;
+                        Monitor.Pulse(lck);
+                    }
+                    finally
+                    {
+                        Monitor.Exit(lck);
+                    }
+                }
+                else
+                {
+                    success = false;
+                    done = true;
+                }
+            });
+            // wait for the two tasks to be executed
+            lock (lck)
+            {
+                if (!done)
+                {
+                    Check.That(Monitor.Wait(lck, 100));
+                }
+            }
+            Check.That(done).IsTrue();
+            Check.That(success).IsTrue();
+        }
+
+        private ISequencer BuildSequencer(IUnitOfExecution synchExec)
+        {
             var sequencer = this.Constructor.Invoke(new object[] {synchExec}) as ISequencer;
 
             Check.That(sequencer).IsNotNull();
-
-            sequencer.Dispatch( () => {});
-
-            Check.That(synchExec._doneTasks).Equals(1);
+            return sequencer;
         }
     }
 }
