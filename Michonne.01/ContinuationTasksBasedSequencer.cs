94556af7bdbf889920e7307284aa1f4ef3532a5f
Thomas PIERRAIN <thomas@pierrain.net>
2014-09-13 09:31:39 +0200

    Set the proper file headers (with proper authors) within the Michonne.01 project.

--- Michonne.01/ContinuationTasksBasedSequencer.cs
+++ Michonne.01/ContinuationTasksBasedSequencer.cs
@@ -1,17 +1,17 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ContinuationTasksBasedSequencer.cs" company="No lock... no deadlock">
-//   Copyright 2014 Thomas PIERRAIN
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//       http://www.apache.org/licenses/LICENSE-2.0
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="BalkingDispatcher.cs" company="No lock... no deadlock" product="Michonne">
+//     Copyright 2014 Olivier COANET (@ocoanet)
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//         http://www.apache.org/licenses/LICENSE-2.0
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+//   </copyright>
+//   --------------------------------------------------------------------------------------------------------------------
 namespace Michonne
 {
     using System;
--- Michonne.01/DirectDispatcher.cs
+++ Michonne.01/DirectDispatcher.cs
@@ -1,21 +1,22 @@
 // --------------------------------------------------------------------------------------------------------------------
-// <copyright file="DirectDispatcher.cs" company="No lock... no deadlock">
-//   Copyright 2014 Thomas PIERRAIN
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//       http://www.apache.org/licenses/LICENSE-2.0
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="BalkingDispatcher.cs" company="No lock... no deadlock" product="Michonne">
+//     Copyright 2014 Cyrille  DUPUYDAUBY (@Cyrdup), Thomas PIERRAIN (@tpierrain)
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//         http://www.apache.org/licenses/LICENSE-2.0
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+//   </copyright>
+//   --------------------------------------------------------------------------------------------------------------------
 namespace Michonne
 {
     using System;
-    using Interfaces;
+
+    using Michonne.Interfaces;
 
     public sealed class DirectDispatcher : IUnitOfExecution
     {
