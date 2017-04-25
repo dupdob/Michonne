e1620c5dfdae07592a2c4a5fc53630c9451e28ed
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2017-04-25 15:16:35 +0200

    Versionning #1

--- Solution/Version.cs
+++ Solution/Version.cs
@@ -29,3 +29,4 @@ using System.Reflection;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.4.0.*")]
 [assembly: AssemblyFileVersion("0.4.0.*")]
+[assembly: AssemblyInformationalVersion("0.4.0.*-alpha")]
